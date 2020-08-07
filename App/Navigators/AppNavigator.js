3379a1f70ade966ebe4633141ea40dc745552bf4
Diksha <diksha1995@gmail.com>
2020-08-07 12:51:51 +0530

    added2

--- App/Navigators/AppNavigator.js
+++ App/Navigators/AppNavigator.js
@@ -44,6 +44,7 @@ function AppNavigator() {
     <NavigationContainer>
       {/* <StackNavigator.Navigator initialRouteName={intialRout} screenOptions={{ headerShown: false }}> */}
       <StackNavigator.Navigator initialRouteName={'LogIn'} screenOptions={{ headerShown: false }}>
+        <StackNavigator.Screen name="LogIn" component={LoginIn} />
         <StackNavigator.Screen name="Tasks" component={Tasks} />
         <StackNavigator.Screen name="SignUp" component={SignUp} />
         <StackNavigator.Screen name="ChangePassord" component={ChangePassord} />
