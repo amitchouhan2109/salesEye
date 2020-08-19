e9ffe6fff3791b1220c690406cc6ec0885ea9e64
Diksha <diksha1995@gmail.com>
2020-08-19 23:07:26 +0530

    changed apk name

--- android/app/src/main/java/com/taskerium/MainActivity.java
+++ android/app/src/main/java/com/taskerium/MainActivity.java
@@ -1,4 +1,4 @@
-package com.saleseyes;
+package com.taskerium;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SalesEyes";
+    return "Taskerium";
   }
 }
